b42a885820378f47a9adeb4830e81146a150518b
roger254 <rogerbrian.mutua@gmail.com>
2018-12-14 16:38:53 +0300

    Added Connection Reliability

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,26 @@
+from urllib.error import HTTPError
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 
 
-html = urlopen('http://www.pythonscraping.com/exercises/exercise1.html')
-bsObj = BeautifulSoup(html.read())
-print(bsObj.h1)
+# catch 404 and server error
+def get_title(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+
+    try:
+        bs_obj = BeautifulSoup(html.read())
+        title_ = bs_obj.body.h1
+    except AttributeError:
+        return None
+    return title_
+
+
+title = get_title('http://www.pythonscraping.com/exercises/exercise1.html')
+if title is None:
+    print('Title could not be found')
+else:
+    print(title)
