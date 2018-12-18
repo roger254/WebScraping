c5a779a4d9da1796809ac28823ca8209397c1e3c
roger254 <rogerbrian.mutua@gmail.com>
2018-12-18 14:42:20 +0300

    connect to mysql using pymsql

--- scrapper.py
+++ scrapper.py
@@ -1,57 +1,15 @@
-import os
-from urllib.error import HTTPError
-from urllib.request import urlopen, urlretrieve
-from bs4 import BeautifulSoup
+import pymysql
 
-download_dir = 'download'
-base_url = 'http://pythonscraping.com'
+conn = pymysql.connect(
+    host='127.0.0.1',
+    user='roger',
+    password='r15mysql',
+    db='mysql'
+)
 
-
-# catch 404 and server error
-def get_url(url):
-    try:
-        html_ = urlopen(url)
-    except HTTPError:
-        return None
-    return html_
-
-
-def get_absolute_url(base_url_, source):
-    if source.startswith('http://www.'):
-        url = 'http://' + source[11:]
-    elif source.startswith('http://'):
-        url = source
-    elif source.startswith('www.'):
-        url = source[4:]
-        url = 'http://' + source
-    else:
-        url = base_url_ + '/' + source
-    if base_url_ not in url:
-        return None
-    return url
-
-
-def get_download_path(base_url_, absolute_url, download_dir_):
-    path = absolute_url.replace('www.', '')
-    path = path.replace(base_url_, '')
-    path = download_dir_ + path
-    directory = os.path.dirname(path)
-
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    return path
-
-
-html = get_url('http://www.pythonscraping.com')
-bsc_Obj = BeautifulSoup(html, 'html.parser')
-download_list = bsc_Obj.findAll(src=True)
-
-for download in download_list:
-    file_url = get_absolute_url(base_url, download['src'])
-    if file_url is not None:
-        print(file_url)
-        urlretrieve(
-            file_url,
-            get_download_path(base_url, file_url, download_dir)
-        )
+cur = conn.cursor()
+cur.execute('USE scraping')
+cur.execute('SELECT * FROM pages WHERE id=2')
+print(cur.fetchone())
+cur.close()
+conn.close()
