25ed11cb6eca8d27f589a274fc287a9a6a263d33
roger254 <rogerbrian.mutua@gmail.com>
2018-12-18 09:32:59 +0300

    get the ip address from the wikipedia edits and maps them with their countries from the http://api.ipstack.com/ api

--- scrapper.py
+++ scrapper.py
@@ -1,9 +1,12 @@
+import json
 from urllib.error import HTTPError
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 import datetime
 import random
+
+random.seed(datetime.datetime.now())
 
 
 # catch 404 and server error
@@ -15,63 +18,53 @@ def get_url(url):
     return html_
 
 
-# ensure no duplicate links
-pages = set()
-random.seed(datetime.datetime.now())
-
-
-# retrieve a list of all Internal links found on a page
-def get_internal_links(bs_obj, include_url):
-    internal_links = []
-    # finds all links that begin with  '/'
-    expression = re.compile("^(/|.*" + include_url + ")")
-    for link in bs_obj.find_all('a', href=expression):
-        if link.attrs['href'] is not None:
-            if link.attrs['href'] not in internal_links:
-                internal_links.append(link.attrs['href'])
-
-    return internal_links
-
-
-# retrieves a list of all external links found in a page
-def get_external_links(bs_obj, exclude_url):
-    external_links = []
-    # finds all links that start with 'http' or 'www' that do not
-    # contain the current url
-    expression = re.compile("^(http|www)((?!" + exclude_url + ").)*$")
-    found = bs_obj.findAll('a', href=expression)
-    for link in found:
-        if link.attrs['href'] is not None:
-            if link.attrs['href'] not in external_links:
-                external_links.append(link.attrs['href'])
-
-    return external_links
-
-
-def split_address(address):
-    address_parts = address.replace('https://', "").split("/")
-    return address_parts
-
-
-def get_random_external_link(starting_page):
-    html = get_url(starting_page)
+def get_links(article_url):
+    html = get_url('http://en.wikipedia.org' + article_url)
     bs_obj = BeautifulSoup(html, 'html.parser')
-    external_links = get_external_links(bs_obj, split_address(starting_page)[0])
-    if len(external_links) == 0:
-
-        internal_links = get_internal_links(starting_page)
-
-        return get_random_external_link(
-            internal_links[random.randint(0, len(internal_links) - 1)]
-        )
-    else:
-        return external_links[random.randint(0, len(external_links) - 1)]
+    expression = re.compile("^(/wiki/)((?!:).)*$")
+    return bs_obj.find('div', {'id': 'bodyContent'}).findAll('a', href=expression)
 
 
-def follow_external_only(starting_page):
-    external_link = get_random_external_link(starting_page)
-    print('Random external link is: ' + external_link)
-    follow_external_only(external_link)
+def get_history_ips(page_url):
+    # Format of revision history pages
+    # http://en.wikipedia.org/w/index.php?title=Title_in_URL&action=history
+    page_url = page_url.replace('/wiki/', '')
+    history_url = 'http://en.wikipedia.org/w/index.php?title=' + page_url + '&action=history'
+    print('History url is' + history_url)
+    html = get_url(history_url)
+    bs_obj = BeautifulSoup(html, 'html.parser')
+    # finds only the links with class "mw-anonuserlink" which has IP addresses
+    # instead of usernames
+    ip_addresses = bs_obj.find_all('a', {'class': 'mw-anonuserlink'})
+    address_list = set()
+    for ip_address in ip_addresses:
+        address_list.add(ip_address.get_text())
+
+    return address_list
 
 
-follow_external_only('https://oreilly.com')
+def get_country(ip_address):
+    api_access = '0a41a2c00229ea80b86a87ef0ef1806e'
+
+    response = get_url(
+        'http://api.ipstack.com/' +
+        ip_address +
+        '?access_key=' + api_access
+    )
+
+    response_json = json.loads(response.read().decode('utf-8'))
+    return response_json.get('country_code'), response_json.get('country_name')
+
+
+links = get_links('/wiki/Python_(programming_language)')
+while len(links) > 0:
+    for link in links:
+        print('-' * 20)
+        history_ips = get_history_ips(link.attrs['href'])
+        for history_ip in history_ips:
+            country_code, country = get_country(history_ip)
+            if country_code is not None and country is not None:
+                print(history_ip + ' is from ' + country_code + '(' + country + ')')
+
+    new_link = links[random.randint(0, len(links) - 1)].attrs['href']
+    links = get_links(new_link)
