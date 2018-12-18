801d7015d2a7613a1330cba0b846c077fb648a85
roger254 <rogerbrian.mutua@gmail.com>
2018-12-18 18:41:04 +0300

    scrapped wiki for title and content and stored in the database

--- scrapper.py
+++ scrapper.py
@@ -1,15 +1,57 @@
 import pymysql
+import datetime
+import random
+import re
+from urllib.request import urlopen, HTTPError
+from bs4 import BeautifulSoup
 
 conn = pymysql.connect(
     host='127.0.0.1',
-    user='roger',
-    password='r15mysql',
+    user='',
+    password='',
     db='mysql'
 )
 
 cur = conn.cursor()
 cur.execute('USE scraping')
-cur.execute('SELECT * FROM pages WHERE id=2')
-print(cur.fetchone())
-cur.close()
-conn.close()
+
+random.seed(datetime.datetime.now())
+
+
+def store(title, content):
+    cur.execute(
+        'INSERT INTO pages (title, content) VALUES (%s,%s)',
+        (title, content)
+    )
+    cur.connection.commit()
+
+
+def get_url(url):
+    print(url)
+    try:
+        html = urlopen(url)
+    except HTTPError:
+        return None
+    return html
+
+
+def get_links(article_url):
+    html = get_url('http://en.wikipedia.org' + article_url)
+    bs_obj = BeautifulSoup(html, 'html.parser')
+    title = bs_obj.find('h1').get_text()
+    content = bs_obj.find('div', {'id': 'mw-content-text'}).find('p').get_text()
+    if len(content) > 1:
+        store(title, content)
+    expression = re.compile('^(/wiki/)((?!:).)*$')
+    return bs_obj.find('div', {'id': 'bodyContent'}).findAll('a', href=expression)
+
+
+links = get_links('/wiki/Kevin_Bacon')
+try:
+    while len(links) > 0:
+        new_article = links[random.randint(0, len(links) - 1)].attrs['href']
+        print(new_article)
+        links = get_links(new_article)
+finally:
+    cur.close()
+    conn.close()
